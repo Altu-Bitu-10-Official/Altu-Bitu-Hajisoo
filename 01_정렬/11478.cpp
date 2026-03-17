358c5cd4e0b3732b118adecfeddf1a4ca052373d
jisoo0402 <2466058@ewha.ac.kr>
2026-03-17 18:57:35 +0900

    [정렬] 3월 17일 - Update

--- 01_정렬/11478.cpp
+++ 01_정렬/11478.cpp
@@ -8,7 +8,6 @@ int check(string st) {
 	set<string> s;
 	int len = st.length();
 	for (int i = 0; i < len; i++) {
-		for (int j = len - i; j >= 1; j--) {
 		for (int j = 1; j <= len - i; j++) {
 			string substring = st.substr(i, j);
 			s.insert(substring);
