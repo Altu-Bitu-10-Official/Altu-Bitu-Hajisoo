ac9bb9eb458a7bd1fac208f6f82fd90468b4324c
jisoo0402 <2466058@ewha.ac.kr>
2026-05-12 13:09:45 +0900

    [동적계획법] 5월 12일

---
+++ 07_동적계획법/11053.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    vector<int> dp(n, 1);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (arr[j] < arr[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    int answer = 0;
+    for (int i = 0; i < n; i++) {
+        answer = max(answer, dp[i]);
+    }
+
+    cout << answer;
+
+    return 0;
+}
---
+++ 07_동적계획법/11726.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1, 0);
+
+    dp[1] = 1;
+
+    if (n >= 2) {
+        dp[2] = 2;
+    }
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+
+    cout << dp[n];
+
+    return 0;
+}
